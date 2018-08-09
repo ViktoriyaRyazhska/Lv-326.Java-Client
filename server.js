c0a3a6659a18fe488b161ba3e886256b26c2705c
Vova Ovcharenko <Volodymyyrr@gmail.com>
2018-08-09 15:00:43 +0300

    Some shit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  // res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
