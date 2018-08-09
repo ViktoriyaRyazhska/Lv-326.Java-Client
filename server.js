ab83750850ee29afea6900290d3123185169399a
Vova Ovcharenko <Volodymyyrr@gmail.com>
2018-08-09 15:14:23 +0300

    Some shit

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files form the dist directory
+// app.use(express.static());
+//
+// app.get('/*', function(req,res) {
+//
+//   // res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// });
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 3000);
 
-const app = express();
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require("./routes");
 
-// Serve only the static files form the dist directory
-app.use(express.static('/'));
+var app = express();
 
-app.get('/*', function(req,res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  // res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use("/api",routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+module.exports = app;
